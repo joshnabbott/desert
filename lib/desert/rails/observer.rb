384000649c1e26738d8412f275d7a9f9582537f9
Aitor García <aitor.garcia@gmail.com>
2008-08-29 12:10:35 +0200

    Desert::Rails::Observer added to allow plugins to register observers on startup.

---
+++ lib/desert/rails/observer.rb
@@ -0,0 +1,21 @@
+module Desert
+  module Rails
+    module Observer
+      def self.observers=(*observers)
+        @observers = observers.flatten
+      end
+      def self.observers
+        @observers ||= []
+      end
+    end
+  end
+end
+
+class Rails::Initializer
+  def load_observers_with_desert
+    puts  "Adding #{Desert::Rails::Observer.observers.inspect} to #{ActiveRecord::Base.observers.inspect}"
+    ActiveRecord::Base.observers += Desert::Rails::Observer.observers.uniq
+    load_observers_without_desert
+  end
+  alias_method_chain :load_observers, :desert
+end
