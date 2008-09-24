6eb380888a9eaca3a172cfd01ca5c49fb633ea73
Pedro Belo <pedro@bitscribe.net>
2008-09-23 17:36:41 -0700

    avoid trying to create the table when it already exists; that will invalidate any current transaction

--- lib/desert/plugin_migrations/extensions/2.1/schema_statements.rb
+++ lib/desert/plugin_migrations/extensions/2.1/schema_statements.rb
@@ -2,11 +2,7 @@ ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
   def initialize_schema_migrations_table_with_plugins
     initialize_schema_migrations_table_without_plugins
 
-    begin
-      execute "CREATE TABLE #{Desert::PluginMigrations::Migrator.schema_info_table_name} (plugin_name #{type_to_sql(:string)}, version #{type_to_sql(:integer)})"
-    rescue ActiveRecord::StatementInvalid
-      # Schema has been initialized
-    end
+    execute "CREATE TABLE #{Desert::PluginMigrations::Migrator.schema_info_table_name} (plugin_name #{type_to_sql(:string)}, version #{type_to_sql(:integer)})" unless tables.include?(Desert::PluginMigrations::Migrator.schema_info_table_name)
   end
   alias_method_chain :initialize_schema_migrations_table, :plugins
 end
