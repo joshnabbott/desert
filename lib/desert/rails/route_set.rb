602d1e3ec33f09da3f39a60fbdd2d5aa0bb2fc56
joshnabbott <joshnabbott@mac.com>
2009-05-13 16:21:13 -0700

    Use plugin_name/config/desert_routes.rb file for mixin routes.

--- lib/desert/rails/route_set.rb
+++ lib/desert/rails/route_set.rb
@@ -4,12 +4,12 @@ module Desert
       # Loads the set of routes from within a plugin and evaluates them at this
       # point within an application's main <tt>routes.rb</tt> file.
       #
-      # Plugin routes are loaded from <tt><plugin_root>/routes.rb</tt>.
+      # Plugin routes are loaded from <tt><plugin_root>/desert_routes.rb</tt>.
       def routes_from_plugin(name)
         name = name.to_s
         routes_path = File.join(
           Desert::Manager.plugin_path(name),
-          "config/routes.rb"
+          "config/desert_routes.rb"
         )
         RAILS_DEFAULT_LOGGER.debug "Loading routes from #{routes_path}."
         eval(IO.read(routes_path), binding, routes_path) if File.file?(routes_path)
